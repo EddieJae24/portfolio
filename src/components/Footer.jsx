e9eb2eb007af714d7570505f3824d05a203d213a
Edward Ofori <tetraed.llc@gmail.com>
2024-11-12 16:44:31 -0500

    added files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// src/components/Footer.jsx
+function Footer() {
+    return (
+      <footer>
+        <a href="https://github.com/EddieJae24" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">Twitter</a>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
---
+++ src/components/Projects.jsx
@@ -0,0 +1,16 @@
+// src/components/Project.jsx
+function Project({ title, image, appLink, repoLink }) {
+    return (
+      <div className="project">
+        <img src={image} alt={`${title} screenshot`} />
+        <h3>{title}</h3>
+        <div className="links">
+          <a href={appLink} target="_blank" rel="noopener noreferrer">Deployed App</a>
+          <a href={repoLink} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Project;
+  
