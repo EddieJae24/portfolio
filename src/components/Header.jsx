42fc7b62a51a8b8a6b2348ba735dfffa4617a4cc
Edward Ofori <tetraed.llc@gmail.com>
2024-11-12 16:41:09 -0500

    added files

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+// src/components/Header.jsx
+import Navigation from './Navigation';
+
+function Header() {
+  return (
+    <header>
+      <h1>Developer's Name</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
